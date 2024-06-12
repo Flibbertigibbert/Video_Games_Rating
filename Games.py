d638b6acee41a19d502ddae448a720c258edab0c
Adebola Rabiu <debolamech@gmail.com>
2024-06-12 21:13:04 +0100

    Add files via upload

--- Games.py
+++ Games.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
-import sklearn
+import scikit-learn
 import category_encoders
 from sklearn.pipeline import make_pipeline
 
